cf60ccf1a27f7b32e7a4ccec48065482765ec2b4
Linhyum <phiep860974@gmail.com>
2023-07-25 15:13:36 +0700

    fix bug main layout

--- src/layouts/MainLayout/MainLayout.stories.tsx
+++ src/layouts/MainLayout/MainLayout.stories.tsx
@@ -6,12 +6,6 @@ const meta = {
    component: MainLayout,
    tags: ['autodocs'],
    argTypes: {
-      children: {
-         description: 'Nội dung của trang web',
-         table: {
-            type: { summary: 'React.ReactNode' }
-         }
-      },
       isRegister: {
          description: 'Kiểm tra có phải là header của trang register hay không'
       },
@@ -24,17 +18,4 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
-   args: {
-      children: 'Nội dung của trang web'
-   }
-}
-
-// Primary.story = {
-//    parameters: {
-//       reactRouter: {
-//          routePath: '/:id',
-//          routeParams: { id: 'Điện-Thoại-Vsmart-Active-3-6GB64GB--Hàng-Chính-Hãng-i-60afb2c76ef5b902180aacba' }
-//       }
-//    }
-// }
+export const Primary: Story = {}
